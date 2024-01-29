019e915f95dc6d84b90ad3962a3b8653b64d8919
BryhanS <bryhansalazarpacsi@gmail.comm>
2024-01-29 01:12:49 -0500

    cartRouter finish

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express();
 const PUERTO = 8080;
 
 const productRouter = require("./routes/products.router.js");
+const cartRouter = require("./routes/cart.router.js");
 
 // const exp = require("constants");
 
@@ -16,5 +17,6 @@ app.get("/", (req, res) => {
 //Routes
 
 app.use("/api/products", productRouter);
+app.use("/api/carts", cartRouter);
 
 app.listen(PUERTO);
