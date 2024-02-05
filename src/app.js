a629f97b23ed95f47e5e5b153dd854573eed43df
BryhanS <bryhansalazarpacsi@gmail.comm>
2024-02-04 23:50:30 -0500

    routerHome

--- src/app.js
+++ src/app.js
@@ -1,22 +1,32 @@
 const express = require("express");
 const app = express();
 const PUERTO = 8080;
+const exphbs = require("express-handlebars");
 
-const productRouter = require("./routes/products.router.js");
-const cartRouter = require("./routes/cart.router.js");
-
+// const productRouter = require("./routes/products.router.js");
+// const cartRouter = require("./routes/cart.router.js");
 // const exp = require("constants");
+
+const viewRouter = require("./routes/views.router.js");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Gracias por ingresar a mi nueva APP!!!");
-});
+app.use(express.static("./src/public"));
+
+//config handlebars
+
+app.engine("handlebars", exphbs.engine());
+
+app.set("view engine", "handlebars");
+
+app.set("views", "./src/views");
 
 //Routes
+// app.use("/api/products", productRouter);
+// app.use("/api/carts", cartRouter);
+app.use("/", viewRouter);
 
-app.use("/api/products", productRouter);
-app.use("/api/carts", cartRouter);
-
-app.listen(PUERTO);
+app.listen(PUERTO, () => {
+  console.log(`Escuchando en el puerto ${PUERTO}`);
+});
